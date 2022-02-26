fe035c33fce0af515d236d159584902b1badfe89
Marco Rettore <marcorettoremattes@gmail.com>
2022-02-26 12:34:18 -0300

    Day 4 of practice

--- src/es9/index.js
+++ src/es9/index.js
@@ -22,3 +22,32 @@ let { country, ...all} = obj;
 
 console.log(all);
 
+const data = 
+{
+  name: 'Marco',
+  age: 27,
+}
+
+const data2 =
+{
+  ...data,
+  country: 'AR',
+}
+
+console.log(data2);
+
+const helloWorld = () =>
+{
+  return new Promise((resolve, reject) =>
+  {
+    true
+    ? setTimeout(() =>resolve('Hola, está ok'), 3000)
+    : reject(new Error('No está ok'))
+  })
+};
+
+helloWorld()
+  .then(response => console.log(response))
+  .catch(error => console.log(error))
+  .finally(() => console.log('Finalizado'))
+
